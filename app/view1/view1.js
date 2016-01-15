0ddb44a815b224f67110c60700f1ff03e3a991bf
Jason Hendry <Jason@portablestudios.com.au>
2016-01-15 17:20:55 +1100

    Add Export, add ngDraggable module

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,17 +10,29 @@ angular.module('myApp.view1', ['ngRoute'])
     }])
 
     .controller('View1Ctrl', ['$scope', function ($scope) {
-
+        $scope.startTime = localStorage.getItem('startTime');
+        if ($scope.startTime == null) {
+            $scope.startTime = '9:00';
+        }
+        $scope.export = '';
         $scope.save = function () {
             localStorage.setItem('timeslots', JSON.stringify($scope.timeslots));
             localStorage.setItem('backlog', JSON.stringify($scope.backlog));
+            $scope.export = '';
+            for (var i in $scope.timeslots) {
+                if (!$scope.timeslots.hasOwnProperty(i)) {
+                    continue;
+                }
+                var s = $scope.timeslots[i];
+                $scope.export += s.time.getHours() + ':' + s.time.getMinutes() + ' [' + (s.completed ? 'x' : ' ') + '] ' + s.item + '\n';
+            }
         };
         $scope.addToDay = function (item) {
             $scope.addToSlot(item);
             $scope.redoTimes();
             $scope.backlog.splice($scope.backlog.indexOf(item), 1);
         };
-        $scope.removeItem = function(item) {
+        $scope.removeItem = function (item) {
             $scope.backlog.splice($scope.backlog.indexOf(item), 1);
         };
         $scope.moveToBacklog = function (slot) {
@@ -28,9 +40,33 @@ angular.module('myApp.view1', ['ngRoute'])
             $scope.timeslots.splice($scope.timeslots.indexOf(slot), 1);
             $scope.redoTimes();
         };
+        $scope.duplicate = function (slot) {
+            $scope.addToSlot(slot.item);
+            $scope.redoTimes();
+        };
+        $scope.startHour = function () {
+            return $scope.startTime.split(':')[0];
+        };
+        $scope.startMinute = function () {
+            return $scope.startTime.split(':')[1];
+        };
+        $scope.newDay = function() {
+            for (var i=0;i<$scope.timeslots.length;) {
+                if (!$scope.timeslots.hasOwnProperty(i)) {
+                    continue;
+                }
+                var s=$scope.timeslots[i];
+                if(s.completed) {
+                    $scope.timeslots.splice(i, 1);
+                } else {
+                    i++;
+                }
+            }
+            $scope.redoTimes();
+        };
         $scope.redoTimes = function () {
             var startTime = new Date();
-            startTime.setHours(9, 0, 0);
+            startTime.setHours($scope.startHour(), $scope.startMinute(), 0);
             for (var i in $scope.timeslots) {
                 if (!$scope.timeslots.hasOwnProperty(i)) {
                     continue;
@@ -75,7 +111,13 @@ angular.module('myApp.view1', ['ngRoute'])
         }
         $scope.redoTimes();
 
-        $scope.$watchCollection('backlog',$scope.save);
-        $scope.$watchCollection('timeslots',$scope.save);
-
+        $scope.$watchCollection('backlog', $scope.save);
+        $scope.$watchCollection('timeslots', $scope.save);
+        $scope.$watch('startTime', function () {
+            if (!$scope.startTime.match(/[0-9]{1,2}:[0-9]{2}/)) {
+                $scope.startTime = '9:00';
+            }
+            localStorage.setItem('startTime', $scope.startTime);
+            $scope.redoTimes();
+        });
     }]);
